5ee68d00d7a758aa1262fe0cba60fc9508d3a3f7
Mensah Michael <msquaremega@gmail.com>
2020-05-31 03:27:18 +0100

    set all token control functions

--- src/app/shared/services/token/token.service.ts
+++ src/app/shared/services/token/token.service.ts
@@ -1,9 +1,56 @@
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TokenService {
 
+  private iss = {
+    url: environment.url
+  };
+
   constructor() { }
+
+  set(token: any) {
+    localStorage.setItem('token', token.access_token);
+  }
+
+  get() {
+    return localStorage.getItem('token');
+  }
+
+  remove() {
+    localStorage.removeItem('token');
+  }
+
+  handle(token: any) {
+    this.set(token);
+  }
+
+  decode(payload: any) {
+    return JSON.parse(atob(payload));
+  }
+
+  payload(token: any) {
+    if (token) {
+      const payload = token.split('.')[1];
+      return this.decode(payload);
+    }
+  }
+
+  isTokenValid() {
+    const token = this.get();
+    if (token) {
+      const payload = this.payload(token);
+      if (payload) {
+        return Object.values(this.iss).indexOf(payload.iss) > -1 ? true : false;
+      }
+    }
+    return false;
+  }
+
+  loggedIn() {
+    return this.isTokenValid();
+  }
 }
