0f1db58850505ebbde12952f16316353cdbeb325
Mensah Michael <msquaremega@gmail.com>
2020-06-01 19:35:23 +0100

    create set title and get title functions in title service for each page title config

--- src/app/shared/services/title/title.service.ts
+++ src/app/shared/services/title/title.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TitleService {
 
+  private title = new BehaviorSubject<any>('Angular Authentication');
+  private title$ = this.title.asObservable();
+
   constructor() { }
+
+  setTitle(title: any) {
+    this.title.next(title);
+  }
+
+  getTitle(): Observable<any> {
+    return this.title$;
+  }
 }
