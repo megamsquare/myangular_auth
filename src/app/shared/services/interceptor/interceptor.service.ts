51465f493b2a18421704fd3aa0e4cb8a74c125df
Mensah Michael <msquaremega@gmail.com>
2020-06-02 23:12:12 +0100

    generate interceptor sevice

---
+++ src/app/shared/services/interceptor/interceptor.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@angular/core';
+import { HttpInterceptor } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class InterceptorService {
+
+  constructor() { }
+}
