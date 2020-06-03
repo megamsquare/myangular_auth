2d5b362bd8b634f40d167dd1751b6335c37868a1
Mensah Michael <msquaremega@gmail.com>
2020-06-03 02:02:15 +0100

    change status of logged in user function

--- src/app/shared/services/auth/auth.service.ts
+++ src/app/shared/services/auth/auth.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { TokenService } from '../token/token.service';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  private loggedIn = new BehaviorSubject<boolean>(this.token.loggedIn());
+  authStatus = this.loggedIn.asObservable();
+
+  constructor(private token: TokenService) { }
+
+  changeAuthStatus(value: boolean) {
+    this.loggedIn.next(value);
+  }
 }
