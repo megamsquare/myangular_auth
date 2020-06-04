6bd8f232559ce882f6d0eeec852917908980787b
Mensah Michael <msquaremega@gmail.com>
2020-06-04 04:54:32 +0100

    canActivate is implemented into before login service for login control

--- src/app/shared/services/login/before-login/before-login.service.ts
+++ src/app/shared/services/login/before-login/before-login.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import { CanActivate, Router } from '@angular/router';
+import { TokenService } from '../../token/token.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class BeforeLoginService {
+export class BeforeLoginService implements CanActivate {
 
-  constructor() { }
+  constructor(
+    private token: TokenService,
+    private router: Router
+  ) { }
+
+  // tslint:disable-next-line: max-line-length
+  canActivate(route: import('@angular/router').ActivatedRouteSnapshot, state: import('@angular/router').RouterStateSnapshot): boolean | import('@angular/router').UrlTree | import('rxjs').Observable<boolean | import('@angular/router').UrlTree> | Promise<boolean | import('@angular/router').UrlTree> {
+    if (!this.token.loggedIn()) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 }
