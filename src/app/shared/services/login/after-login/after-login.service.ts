712a1ab1c7c54ad180e4f7fb85c42f63f0d54787
Mensah Michael <msquaremega@gmail.com>
2020-06-04 04:44:32 +0100

    canActivate is implemented into after login service for login control

--- src/app/shared/services/login/after-login/after-login.service.ts
+++ src/app/shared/services/login/after-login/after-login.service.ts
@@ -1,9 +1,31 @@
 import { Injectable } from '@angular/core';
+import { CanActivate, CanActivateChild, Router } from '@angular/router';
+import { TokenService } from '../../token/token.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AfterLoginService {
+export class AfterLoginService implements CanActivate, CanActivateChild {
 
-  constructor() { }
+  constructor(
+    private token: TokenService,
+    private router: Router
+  ) { }
+
+  // tslint:disable-next-line: max-line-length
+  canActivate(route: import('@angular/router').ActivatedRouteSnapshot, state: import('@angular/router').RouterStateSnapshot): boolean | import('@angular/router').UrlTree | import('rxjs').Observable<boolean | import('@angular/router').UrlTree> | Promise<boolean | import('@angular/router').UrlTree> {
+
+    if (this.token.loggedIn()) {
+      return true;
+    } else {
+      this.token.remove();
+      this.router.navigateByUrl('/');
+      return false;
+    }
+
+  }
+  // tslint:disable-next-line: max-line-length
+  canActivateChild(childRoute: import('@angular/router').ActivatedRouteSnapshot, state: import('@angular/router').RouterStateSnapshot): boolean | import('@angular/router').UrlTree | import('rxjs').Observable<boolean | import('@angular/router').UrlTree> | Promise<boolean | import('@angular/router').UrlTree> {
+    throw new Error('Method not implemented.');
+  }
 }
