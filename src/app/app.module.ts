778048643829ae84315b22d7bf8eb90d62df31d5
Mensah Michael <msquaremega@gmail.com>
2020-06-05 23:21:30 +0100

    generate component for pages layout, eg home, about us, etc

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PagesComponent } from './layout/pages/pages.component';
+import { PageHeaderComponent } from './layout/pages/page-header/page-header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PagesComponent,
+    PageHeaderComponent
   ],
   imports: [
     BrowserModule,
