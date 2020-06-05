3710b66214b0265dfa7644f890bed72813bb80df
Mensah Michael <msquaremega@gmail.com>
2020-06-05 23:35:40 +0100

    add pages layout component to app route for routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PagesComponent } from './layout/pages/pages.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: PagesComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
