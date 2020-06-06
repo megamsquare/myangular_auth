faca8451483efe422756865d6547dddead4028b0
Mensah Michael <msquaremega@gmail.com>
2020-06-06 03:06:31 +0100

    user layout header added to user layout and also routing of user layout done on app route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { PagesComponent } from './layout/pages/pages.component';
+import { UserLayoutComponent } from './layout/user-layout/user-layout.component';
 
 
 const routes: Routes = [
   {
     path: '',
     component: PagesComponent
+  },
+  {
+    path: 'home',
+    component: UserLayoutComponent
   }
 ];
 
