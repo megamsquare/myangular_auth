01cf4173b081c008c6199587ca3ad4475fb7f818
Mensah Michael <msquaremega@gmail.com>
2020-06-07 23:59:47 +0100

    config routing for about page

--- src/app/components/pages/about/about-routing.module.ts
+++ src/app/components/pages/about/about-routing.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
+import { AboutComponent } from './about.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: AboutComponent
+  }
+];
 
 
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
 })
 export class AboutRoutingModule { }
