3b7a659fbd16e2ea41f44db19acd150faabb4246
Mensah Michael <msquaremega@gmail.com>
2020-06-07 23:51:50 +0100

    generate about component

--- src/app/components/pages/about/about.module.ts
+++ src/app/components/pages/about/about.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AboutComponent } from './about.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [AboutComponent],
   imports: [
     CommonModule
   ]
