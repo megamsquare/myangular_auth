d05113ddeb4e2601ad6be868aba29c516faca25b
Mensah Michael <msquaremega@gmail.com>
2020-06-09 05:53:29 +0100

    user profile routing module configured for routing user profile and added to user profilr module imports

--- src/app/components/user/user-profile/user-profile.module.ts
+++ src/app/components/user/user-profile/user-profile.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { UserProfileRoutingModule } from './user-profile-routing.module';
 
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    UserProfileRoutingModule
   ]
 })
 export class UserProfileModule { }
