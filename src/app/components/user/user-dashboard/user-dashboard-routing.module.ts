ec0cbdadc215c778679ee1014cc4d0972c9669a7
Mensah Michael <msquaremega@gmail.com>
2020-06-09 05:30:19 +0100

    add user dashboard to user dashboard routing for route purpose

--- src/app/components/user/user-dashboard/user-dashboard-routing.module.ts
+++ src/app/components/user/user-dashboard/user-dashboard-routing.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
+import { UserDashboardComponent } from './user-dashboard.component';
 
+const routes: Routes = [
+  {
+    path: '',
+    component: UserDashboardComponent
+  }
+];
 
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
 })
 export class UserDashboardRoutingModule { }
