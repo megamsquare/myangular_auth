69dbe2c38fb2d66696f43c5e95b35f96c74545eb
Mensah Michael <msquaremega@gmail.com>
2020-06-13 04:17:56 +0100

    adding login to user layout through app routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { PagesComponent } from './layout/pages/pages.component';
 import { UserLayoutComponent } from './layout/user-layout/user-layout.component';
+import { UserAuthComponent } from './layout/auth/user-auth/user-auth.component';
 
 
 const routes: Routes = [
@@ -22,6 +23,16 @@ const routes: Routes = [
       {
         path: 'home',
         loadChildren: () => import('./components/user/user-dashboard/user-dashboard.module').then(m => m.UserDashboardModule)
+      }
+    ]
+  },
+  {
+    path: '',
+    component: UserAuthComponent,
+    children: [
+      {
+        path: 'login',
+        loadChildren: () => import('./components/auth/user-auth/login/login.module').then(m => m.LoginModule)
       }
     ]
   }
