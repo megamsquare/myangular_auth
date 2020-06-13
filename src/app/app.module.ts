fc04280ada3b5251a83ac7ba8b9ae02850a4a5af
Mensah Michael <msquaremega@gmail.com>
2020-06-13 03:40:38 +0100

    changes to auth layout, it has auth layout for user and admin

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,8 @@ import { PagesComponent } from './layout/pages/pages.component';
 import { PageHeaderComponent } from './layout/pages/page-header/page-header.component';
 import { UserLayoutComponent } from './layout/user-layout/user-layout.component';
 import { UserLayoutHeaderComponent } from './layout/user-layout/user-layout-header/user-layout-header.component';
-import { AuthComponent } from './layout/auth/auth.component';
+import { UserAuthComponent } from './layout/auth/user-auth/user-auth.component';
+import { AdminAuthComponent } from './layout/auth/admin-auth/admin-auth.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { AuthComponent } from './layout/auth/auth.component';
     PageHeaderComponent,
     UserLayoutComponent,
     UserLayoutHeaderComponent,
-    AuthComponent
+    UserAuthComponent,
+    AdminAuthComponent
   ],
   imports: [
     BrowserModule,
--- src/app/layout/auth/admin-auth/admin-auth.component.spec.ts
+++ src/app/layout/auth/admin-auth/admin-auth.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { AuthComponent } from './auth.component';
+import { AdminAuthComponent } from './admin-auth.component';
 
-describe('AuthComponent', () => {
-  let component: AuthComponent;
-  let fixture: ComponentFixture<AuthComponent>;
+describe('AdminAuthComponent', () => {
+  let component: AdminAuthComponent;
+  let fixture: ComponentFixture<AdminAuthComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ AuthComponent ]
+      declarations: [ AdminAuthComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(AuthComponent);
+    fixture = TestBed.createComponent(AdminAuthComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
