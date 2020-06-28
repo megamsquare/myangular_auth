476cba991d40e97737fd54fcc6c2e206828e767b
Mensah Michael <msquaremega@gmail.com>
2020-06-28 14:17:55 +0100

    modify team routing module to create routing for team navigation

--- src/app/components/pages/team/team-routing.module.ts
+++ src/app/components/pages/team/team-routing.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
+import { TeamComponent } from './team.component';
 
+const routes: Routes = [
+  {
+    path: '',
+    component: TeamComponent
+  }
+];
 
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
 })
 export class TeamRoutingModule { }
