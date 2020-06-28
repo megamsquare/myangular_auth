d37f92b5a137657253394bc0ccf8a7515da47cd1
Mensah Michael <msquaremega@gmail.com>
2020-06-28 14:12:18 +0100

    generate team components and add them to team module

---
+++ src/app/components/pages/team/team-routing.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-team-routing',
+  templateUrl: './team-routing.component.html',
+  styleUrls: ['./team-routing.component.css']
+})
+export class TeamRoutingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/components/pages/team/team.module.ts
+++ src/app/components/pages/team/team.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TeamRoutingComponent } from './team-routing.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [TeamRoutingComponent],
   imports: [
     CommonModule
   ]
