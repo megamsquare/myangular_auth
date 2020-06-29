fe593b410a101f4873d541f57d27cd55ad301b55
Mensah Michael <msquaremega@gmail.com>
2020-06-29 21:41:46 +0100

    add services routing module to services module

--- src/app/components/pages/services/services.module.ts
+++ src/app/components/pages/services/services.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ServicesComponent } from './services.component';
+import { ServicesRoutingModule } from './services-routing.module';
 
 
 
 @NgModule({
   declarations: [ServicesComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    ServicesRoutingModule
   ]
 })
 export class ServicesModule { }
