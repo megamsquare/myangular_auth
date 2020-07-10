aa2a5be0a6a5ae8f261a2c719932a5141e00eaf5
Mensah Michael <msquaremega@gmail.com>
2020-07-10 14:35:13 +0100

    implement the logic for filter completed

--- src/app/shared/pipe/filter/filter.pipe.ts
+++ src/app/shared/pipe/filter/filter.pipe.ts
@@ -5,8 +5,21 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class FilterPipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(items: any[], searchText: any): any[] {
+
+    if (!items) {
+      return [];
+    }
+
+    if (!searchText) {
+      return items;
+    }
+
+    return items.filter((item: any) => {
+      return Object.keys(item).some((key: any) => {
+        return String(item[key]).toLowerCase().includes(searchText.toLowerCase());
+      });
+    });
   }
 
 }
