223b65801aa44eb65e92bb7649379902820c5f9a
Mensah Michael <msquaremega@gmail.com>
2020-07-10 14:42:24 +0100

    generate date ago

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { FilterPipe } from './pipe/filter/filter.pipe';
+import { DateAgoPipe } from './pipe/date-ago/date-ago.pipe';
 
 
 
 @NgModule({
-  declarations: [FilterPipe],
+  declarations: [FilterPipe, DateAgoPipe],
   imports: [
     CommonModule,
     RouterModule
