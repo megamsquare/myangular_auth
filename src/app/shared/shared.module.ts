d1cc325790ff535ff8e286c9384bfa61725c91ab
Mensah Michael <msquaremega@gmail.com>
2020-07-10 14:24:46 +0100

    generate filter pipe and adding it to shared module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { FilterPipe } from './pipe/filter/filter.pipe';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [FilterPipe],
   imports: [
     CommonModule,
     RouterModule
