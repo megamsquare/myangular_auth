8f6b06e6aac998f4e2dfd498b185705e04c5bb6c
Mensah Michael <msquaremega@gmail.com>
2020-07-14 04:58:21 +0100

    install moment for date format and calling it in date ago pipe

--- src/app/shared/pipe/date-ago/date-ago.pipe.ts
+++ src/app/shared/pipe/date-ago/date-ago.pipe.ts
@@ -1,11 +1,13 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import * as moment from 'moment';
 
 @Pipe({
   name: 'dateAgo'
 })
 export class DateAgoPipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
+  transform(value: string): string {
+    const localDate = moment.utc(value).format();
     return null;
   }
 
