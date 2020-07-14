671ab2b7eb95d006570f68ba3e53ffe70e58a3ee
Mensah Michael <msquaremega@gmail.com>
2020-07-14 05:04:04 +0100

    paartial worked on date ago

--- src/app/shared/pipe/date-ago/date-ago.pipe.ts
+++ src/app/shared/pipe/date-ago/date-ago.pipe.ts
@@ -8,7 +8,16 @@ export class DateAgoPipe implements PipeTransform {
 
   transform(value: string): string {
     const localDate = moment.utc(value).format();
-    return null;
+    if (localDate) {
+      const seconds = Math.floor((+new Date() - +new Date((localDate))) / 1000);
+      if (seconds < 60) {
+        return 'Just now';
+      }
+      const interval = {
+        year: 365 * 24 * 60 * 60
+      };
+    }
+    return localDate;
   }
 
 }
