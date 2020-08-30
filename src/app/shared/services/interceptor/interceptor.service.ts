0e9dc140de3f5ded3b0d44fabd280559c04cb37e
Mensah Michael <msquaremega@gmail.com>
2020-08-30 10:38:56 +0100

    security patches

--- src/app/shared/services/interceptor/interceptor.service.ts
+++ src/app/shared/services/interceptor/interceptor.service.ts
@@ -25,7 +25,18 @@ export class InterceptorService implements HttpInterceptor {
   private addToken(req: HttpRequest<any>, token: string) {
     return req.clone({
       setHeaders: {
-        Authorization: `Bearer ${token}`
+        Authorization: `Bearer ${token}`,
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+        // tslint:disable-next-line: max-line-length
+        'Content-Security-Policy': 'default-src \'self\'; style-src \'self\' fonts.googleapis.com \'unsafe-inline\' maxcdn.bootstrapcdn.com \'unsafe-inline\'; font-src \'self\' fonts.gstatic.com use.fontawesome.com maxcdn.bootstrapcdn.com; script-src \'self\' code.jquery.com www.google-analytics.com ajax.googleapis.com; img-src * \'self\' data: https:; frame-ancestors \'self\'; connect-src \'self\' edubankapi.edubank.ng',
+        'X-Frame-Options': 'SAMEORIGIN',
+        'X-Content-Type-Options': 'nosniff',
+        'Strict-Transport-Security': 'max-age=31536000',
+        // tslint:disable-next-line: max-line-length
+        'Feature-Policy': 'accelerometer \'none\'; gyroscope \'none\'; magnetometer \'none\'; microphone \'none\'; usb \'none\'',
+        'Cache-Control': 'no-cache',
+        Pragma: 'no-cache'
       }
     });
   }
